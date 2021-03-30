4c6b37a3d94006dba0826884d4dc8a2af2b54237
ch-canaza <ch-canaza@hotmail.com>
2021-03-30 16:15:33 -0400

    updating homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   default_url_options host:'localhost:3000'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   root 'welcome#index'
 end
